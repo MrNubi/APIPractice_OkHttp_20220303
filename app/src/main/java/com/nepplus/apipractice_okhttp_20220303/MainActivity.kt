c27849a9adb20346f003c7422ce7c74f1bc47efe
seojungmin <neraize@gmail.com>
2022-03-03 16:20:34 +0900

    binding변수 이용 -> 이벤트 처리 진행-실제 API 서버 연동예정

--- app/src/main/java/com/nepplus/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/MainActivity.kt
@@ -12,5 +12,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupEvents()
+        setVaules()
     }
+
+
+    fun setupEvents(){
+
+        binding.btnLogin.setOnClickListener {
+
+            // id, 비번 추출
+            val inputId = binding.edtId.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+
+            // API 서버에 아이디/ 비번을 보내서 실제로 회원인지 검사 => 로그인시도
+
+
+        }
+    }
+
+    fun setVaules(){
+
+    }
+
 }
