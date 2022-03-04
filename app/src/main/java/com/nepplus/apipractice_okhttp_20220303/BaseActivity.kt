59cbc3f0d8d61fe58abd1611916dd2ae82101786
seojungmin <neraize@gmail.com>
2022-03-04 15:09:59 +0900

    추상메소드 2개를 물려주는 클래스 생성 -추상클래스로 생성(xml 만들 필요없음)

---
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.nepplus.apipractice_okhttp_20220303
+
+import androidx.appcompat.app.AppCompatActivity
+
+// 다른 모든 화면이 공통적으로 가질 기능 / 멤버변수를 모아두는 (부모)클래스
+
+abstract class BaseActivity:AppCompatActivity() {
+
+    // setupEvents / setValues 함수를 만들어두고, 물려주자
+    // 실제 함수를 구현해서 물려줘봐야, 어차피 오버라이딩해서 사용한다
+    //  => 차라리 추상메소드로 물려줘서,  의무로 반드시 오버라이딩하게 만들자
+    abstract fun setupEvents()
+    abstract  fun setValues()
+
+}
