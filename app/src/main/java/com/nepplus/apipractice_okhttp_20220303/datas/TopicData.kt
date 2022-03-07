2948bd445e73c12a71211b6534f681cf74fb4ba4
seojungmin <neraize@gmail.com>
2022-03-07 16:37:20 +0900

    토론주제 표현용 데이터 클래스 추가 - 생성자 x, 멤버변수만 추가 - 서버의 데이터를 참고해서 추가

---
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/datas/TopicData.kt
@@ -0,0 +1,10 @@
+package com.nepplus.apipractice_okhttp_20220303.datas
+
+class TopicData {
+
+    var id = 0  // id는 Int라고 명시
+    var title = ""  // title는  String이라고 명시
+    var imageUrl = ""  // 서버: img_url , 앱: imageUrl  변수명 다른 경우
+    var replyCount = 0 //  서버: reply_count , 앱: replyCount  변수명 다른 경우
+
+}
