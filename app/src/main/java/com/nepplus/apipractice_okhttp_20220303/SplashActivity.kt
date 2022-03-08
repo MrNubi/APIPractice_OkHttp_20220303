3841abbe28d3aacedefe1d9c427aab477676f5c6
seojungmin <neraize@gmail.com>
2022-03-08 09:53:09 +0900

    2.5초 뒤에 진행할 검사, 주석으로 정리

--- app/src/main/java/com/nepplus/apipractice_okhttp_20220303/SplashActivity.kt
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.nepplus.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : BaseActivity() {
 
@@ -19,6 +21,23 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        // 2.5초가 지나면 -> 자동 로그인을 해도되는지? -> 상황에 맞는 화면으로 이동
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+            // 자동로그인을 해도 되는가?
+            // 1) 사용자가 자동로그인 의사를 ok했는지?
+            // 2) 로그인 시에 받아낸 토큰값이 지금도 유효한지?
+            if(){
+                    // 둘다 ok라면, 바로 메인화면으로
+            }
+            else{
+                // 아니라면, 로그인 화면으로
+
+            }
+
+        }, 2500)
 
     }
 }
