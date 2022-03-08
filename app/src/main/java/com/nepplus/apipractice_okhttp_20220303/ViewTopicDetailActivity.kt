d91aa66d0b8429d046518f4f977a317e7a60af6f
seojungmin <neraize@gmail.com>
2022-03-08 10:40:51 +0900

    토론주제 데이터를 들고, 상세화면 진입

--- app/src/main/java/com/nepplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
@@ -4,14 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.nepplus.apipractice_okhttp_20220303.databinding.ActivityViewTopicDetailBinding
+import com.nepplus.apipractice_okhttp_20220303.datas.TopicData
 
-class ViewTopicDetailActivity : AppCompatActivity() {
+class ViewTopicDetailActivity : BaseActivity() {
 
     lateinit var binding : ActivityViewTopicDetailBinding
+
+    // 보여주게될 토론 주제 데이터  -> 이벤트 처리, 데이터 표현등 여러함수에 사용
+    lateinit var mTopicData:TopicData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_view_topic_detail)
+        mTopicData = intent.getSerializableExtra("topic") as TopicData
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
 
     }
 }
