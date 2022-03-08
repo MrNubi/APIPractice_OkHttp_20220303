1ef01855f8ad4ca3e96fb61208595ccd68f28ced
seojungmin <neraize@gmail.com>
2022-03-08 16:41:25 +0900

    댓글 데이터 제작 중 - 보조생성자 개념 체험

---
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/datas/ReplyData.kt
@@ -0,0 +1,24 @@
+package com.nepplus.apipractice_okhttp_20220303.datas
+
+import org.json.JSONObject
+
+class ReplyData(
+   var id: Int,
+   var content: String
+) {
+
+    // 보조 생성자 추가 연습: 파라미터X
+    constructor() : this(0, "내용없음")
+
+    companion object{
+
+        fun getReplyDataFromJson(jsonObj: JSONObject) :ReplyData{
+
+            val replyData = ReplyData()
+
+            // JSON 정보 -> 멤버변수 채우기
+
+            return  replyData
+        }
+    }
+}
