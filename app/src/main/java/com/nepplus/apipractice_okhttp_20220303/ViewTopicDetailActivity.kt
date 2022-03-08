8f540622dc1c259936123e4b01df1bc54cfb6b42
seojungmin <neraize@gmail.com>
2022-03-08 11:54:37 +0900

    json을 넣으면 -> topicData로 변환해주는 함수 추가

--- app/src/main/java/com/nepplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
@@ -41,8 +41,14 @@ class ViewTopicDetailActivity : BaseActivity() {
         ServerUtil.getRequestTopicDetail(mContext, mTopicData.id, object :ServerUtil.JsonResponseHandler{
             override fun onResponse(jsonObject: JSONObject) {
 
+                val dataObj = jsonObject.getJSONObject("data")
+                val topicObj = dataObj.getJSONObject("topic")
 
+                // 토론 정보 JSONObject (topicObj) => TopicData() 형태로 변환 (여러화면에서 진행. 함수로 만들어두자)
+                val topicData = TopicData.getTopicDataFromJson(topicObj)
 
+                // 변환된 객체를, mTopicData로 다시 대입 => UI반영도 다시 실행
+                mTopicData = topicData
             }
         })
     }
--- app/src/main/java/com/nepplus/apipractice_okhttp_20220303/datas/TopicData.kt
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/datas/TopicData.kt
@@ -1,5 +1,6 @@
 package com.nepplus.apipractice_okhttp_20220303.datas
 
+import org.json.JSONObject
 import java.io.Serializable
 
 class TopicData : Serializable{
@@ -9,4 +10,22 @@ class TopicData : Serializable{
     var imageUrl = ""  // 서버: img_url , 앱: imageUrl  변수명 다른 경우
     var replyCount = 0 //  서버: reply_count , 앱: replyCount  변수명 다른 경우
 
+
+    companion object{
+        // 주제 정보를 담고 있는 JSONObject가 들어오면 -> TopicData형태로 변환해주는 함수 => static메소드
+        fun getTopicDataFromJson(jsonObj:JSONObject):TopicData{
+
+            // 기본 내용의 TopicData 생성
+            val topicData = TopicData()
+
+            // jsonObj에서 데이터 추출 -> 멤버변수 대입
+            topicData.id = jsonObj.getInt("id")
+            topicData.title = jsonObj.getString("title")
+            topicData.imageUrl = jsonObj.getString("img_url")
+            topicData.replyCount = jsonObj.getInt("reply_count")
+
+            // 완성된 TopicData 리턴
+            return  topicData
+        }
+    }
 }
