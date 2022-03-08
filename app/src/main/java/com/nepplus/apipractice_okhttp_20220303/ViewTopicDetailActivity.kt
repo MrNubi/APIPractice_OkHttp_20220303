8f12ef4c58b13f66c0d054bde7d385d21819afa6
seojungmin <neraize@gmail.com>
2022-03-08 14:59:56 +0900

    득표현황 새로고침-미리 구조를 잡아둔 함수 활용 getTopicDetailFromServer()

--- app/src/main/java/com/nepplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.nepplus.apipractice_okhttp_20220303.databinding.ActivityViewTopicDetailBinding
@@ -34,8 +35,15 @@ class ViewTopicDetailActivity : BaseActivity() {
             ServerUtil.postRequestVote(mContext, mTopicData.sideList[0].id, object :ServerUtil.JsonResponseHandler{
                 override fun onResponse(jsonObject: JSONObject) {
 
+                    // 토스트로, 서버가 알려준 현재 상황 (신규 투표 OR 재투표 OR 취소 등) 알려주기
+                    val message = jsonObject.getString("message")
 
+                    runOnUiThread {
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                    }
 
+                    // 변경된 득표 현황을 다시 불러오자
+                    getTopicDetailFromServer()
                 }
             })
             // 투표 현황 새로고침(응답)
