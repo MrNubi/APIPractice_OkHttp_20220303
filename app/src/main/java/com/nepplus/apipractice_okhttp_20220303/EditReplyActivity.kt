2f9a87b60fb77cfcca85734b3080dc9523a318cb
seojungmin <neraize@gmail.com>
2022-03-10 12:01:28 +0900

    댓글 작성화면 추가

---
+++ app/src/main/java/com/nepplus/apipractice_okhttp_20220303/EditReplyActivity.kt
@@ -0,0 +1,28 @@
+package com.nepplus.apipractice_okhttp_20220303
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.apipractice_okhttp_20220303.databinding.ActivityEditReplyBinding
+
+class EditReplyActivity : BaseActivity() {
+
+    lateinit var binding: ActivityEditReplyBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_reply)
+
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
